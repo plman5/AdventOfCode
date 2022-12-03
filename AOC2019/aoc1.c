9f54f9ef0c4c38a976c7db2d06d9d794c8c11f8c
plman5 <33044392+plman5@users.noreply.github.com>
2022-12-02 19:58:35 -0500

    Create aoc1.c

---
+++ AOC2019/aoc1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+  int max = 0;
+  int first = 0;
+  int second = 0;
+  int third = 0;
+  int input;
+  int horizontal=0;
+  int depth=0;
+  int aim = 0;
+  int total = 0;
+  char line[100];
+  FILE* inFile = fopen("input1.txt","r");
+    while(fgets(line,sizeof(line),inFile)!= NULL){
+      
+}
+}
